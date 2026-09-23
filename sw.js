009440305c342f703e89940799155cc6c0911260
twessel20 <twessel20@aol.com>
2026-09-23 16:42:02 -0500

    Refresh PWA cache after grouped inventory fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='gfd-aed-pwa-v20';
+const VERSION='gfd-aed-pwa-v21';
 const CORE_CACHE=VERSION+'-core';
 const RUNTIME_CACHE=VERSION+'-runtime';
 const CORE=['./','./index.html','./manifest.webmanifest'];
