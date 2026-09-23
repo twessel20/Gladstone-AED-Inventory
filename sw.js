c191d0e4e722d307146e73ceec5fba0cb7f6cf7c
twessel20 <twessel20@aol.com>
2026-09-23 16:58:54 -0500

    Refresh PWA cache for uniform group pills

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='gfd-aed-pwa-v29';
+const VERSION='gfd-aed-pwa-v30';
 const CORE_CACHE=VERSION+'-core';
 const RUNTIME_CACHE=VERSION+'-runtime';
 const CORE=['./','./index.html','./manifest.webmanifest'];
