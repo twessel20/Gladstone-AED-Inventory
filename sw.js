822024c916238bf180bfe8ff22639bc9e4bae50e
twessel20 <twessel20@aol.com>
2026-09-23 17:11:43 -0500

    Refresh PWA cache for inactive AED workflow

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='gfd-aed-pwa-v35';
+const VERSION='gfd-aed-pwa-v36';
 const CORE_CACHE=VERSION+'-core';
 const RUNTIME_CACHE=VERSION+'-runtime';
 const CORE=['./','./index.html','./manifest.webmanifest'];
