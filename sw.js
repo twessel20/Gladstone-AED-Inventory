c3382db05355e2fb35e5505b0509608f0f20bf3a
twessel20 <twessel20@aol.com>
2026-09-23 17:06:41 -0500

    Refresh PWA cache for dashboard quick scan

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='gfd-aed-pwa-v33';
+const VERSION='gfd-aed-pwa-v34';
 const CORE_CACHE=VERSION+'-core';
 const RUNTIME_CACHE=VERSION+'-runtime';
 const CORE=['./','./index.html','./manifest.webmanifest'];
