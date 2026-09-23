6a930595c5df12c55ab607371835532fd45df7d3
twessel20 <twessel20@aol.com>
2026-09-23 17:05:00 -0500

    Refresh PWA cache for grouped quarterly view

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='gfd-aed-pwa-v32';
+const VERSION='gfd-aed-pwa-v33';
 const CORE_CACHE=VERSION+'-core';
 const RUNTIME_CACHE=VERSION+'-runtime';
 const CORE=['./','./index.html','./manifest.webmanifest'];
