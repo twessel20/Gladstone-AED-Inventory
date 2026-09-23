488e24c2693f9ee223901eac3966822178b6e536
twessel20 <twessel20@aol.com>
2026-09-23 17:43:48 -0500

    Refresh PWA cache for city employee shock tracking

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='gfd-aed-pwa-v48';
+const VERSION='gfd-aed-pwa-v49';
 const CORE_CACHE=VERSION+'-core';
 const RUNTIME_CACHE=VERSION+'-runtime';
 const CORE=['./','./index.html','./manifest.webmanifest'];
