ace0184883b6201b3dab4c25225b68f852824c4c
twessel20 <twessel20@aol.com>
2026-09-23 17:31:38 -0500

    Refresh PWA cache for deployment label update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='gfd-aed-pwa-v41';
+const VERSION='gfd-aed-pwa-v42';
 const CORE_CACHE=VERSION+'-core';
 const RUNTIME_CACHE=VERSION+'-runtime';
 const CORE=['./','./index.html','./manifest.webmanifest'];
