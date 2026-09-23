9fe86fcf5c9cd2dea269b4a4d508ef271fe3fb54
twessel20 <twessel20@aol.com>
2026-09-23 16:56:49 -0500

    Refresh PWA cache for group alert pills

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='gfd-aed-pwa-v27';
+const VERSION='gfd-aed-pwa-v28';
 const CORE_CACHE=VERSION+'-core';
 const RUNTIME_CACHE=VERSION+'-runtime';
 const CORE=['./','./index.html','./manifest.webmanifest'];
