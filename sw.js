fe2b3080b99b7dc07d0b78d1e807a61dc02427fb
twessel20 <twessel20@aol.com>
2026-09-23 16:36:01 -0500

    Refresh PWA cache for AED location label cleanup

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='gfd-aed-pwa-v17';
+const VERSION='gfd-aed-pwa-v18';
 const CORE_CACHE=VERSION+'-core';
 const RUNTIME_CACHE=VERSION+'-runtime';
 const CORE=['./','./index.html','./manifest.webmanifest'];
