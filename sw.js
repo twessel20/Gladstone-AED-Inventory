7f3c52f5836386d91f9fe1b3fc37442aa91a6f1d
twessel20 <twessel20@aol.com>
2026-09-23 16:46:06 -0500

    Refresh PWA cache for collapsible inventory groups

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='gfd-aed-pwa-v22';
+const VERSION='gfd-aed-pwa-v23';
 const CORE_CACHE=VERSION+'-core';
 const RUNTIME_CACHE=VERSION+'-runtime';
 const CORE=['./','./index.html','./manifest.webmanifest'];
