48a41e69a948b510894249b0bc8a7a0cb1fe4be0
twessel20 <twessel20@aol.com>
2026-09-23 16:06:00 -0500

    Refresh PWA cache for mobile alignment fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='gfd-aed-pwa-v10';
+const VERSION='gfd-aed-pwa-v11';
 const CORE_CACHE=VERSION+'-core';
 const RUNTIME_CACHE=VERSION+'-runtime';
 const CORE=['./','./index.html','./gfd-patch.jpg','./manifest.webmanifest'];
