4b7808921747511c5289a723e5ebae61b878f52c
twessel20 <twessel20@aol.com>
2026-09-23 17:39:59 -0500

    Refresh PWA cache for deployment AED chooser

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='gfd-aed-pwa-v46';
+const VERSION='gfd-aed-pwa-v47';
 const CORE_CACHE=VERSION+'-core';
 const RUNTIME_CACHE=VERSION+'-runtime';
 const CORE=['./','./index.html','./manifest.webmanifest'];
