9d58fbd73996fa10177e59d5d0e29ab77b9b92a1
twessel20 <twessel20@aol.com>
2026-09-23 17:36:50 -0500

    Refresh PWA cache for return-to-service workflow

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='gfd-aed-pwa-v44';
+const VERSION='gfd-aed-pwa-v45';
 const CORE_CACHE=VERSION+'-core';
 const RUNTIME_CACHE=VERSION+'-runtime';
 const CORE=['./','./index.html','./manifest.webmanifest'];
