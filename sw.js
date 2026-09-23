326a3946228df92843ce9f80a38a9624af13ff80
twessel20 <twessel20@aol.com>
2026-09-23 17:25:29 -0500

    Refresh PWA cache for agency deployment report tracking

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='gfd-aed-pwa-v40';
+const VERSION='gfd-aed-pwa-v41';
 const CORE_CACHE=VERSION+'-core';
 const RUNTIME_CACHE=VERSION+'-runtime';
 const CORE=['./','./index.html','./manifest.webmanifest'];
