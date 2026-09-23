fc4997dc7ee8dba99e504bc485ce949054a24381
twessel20 <twessel20@aol.com>
2026-09-23 17:07:29 -0500

    Refresh PWA cache for last-check display

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='gfd-aed-pwa-v34';
+const VERSION='gfd-aed-pwa-v35';
 const CORE_CACHE=VERSION+'-core';
 const RUNTIME_CACHE=VERSION+'-runtime';
 const CORE=['./','./index.html','./manifest.webmanifest'];
