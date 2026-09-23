30f2cebaaeda9775cf5f4a3fb2f2e5851b35da10
twessel20 <twessel20@aol.com>
2026-09-23 17:14:27 -0500

    Refresh PWA cache for smart dashboard scanner

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='gfd-aed-pwa-v37';
+const VERSION='gfd-aed-pwa-v38';
 const CORE_CACHE=VERSION+'-core';
 const RUNTIME_CACHE=VERSION+'-runtime';
 const CORE=['./','./index.html','./manifest.webmanifest'];
