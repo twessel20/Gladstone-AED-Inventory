61db775e2fc8bd08b127b64bb76a67ea0641c57a
twessel20 <twessel20@aol.com>
2026-09-24 07:57:26 -0500

    Bump PWA cache to v76

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='gfd-aed-pwa-v75';
+const VERSION='gfd-aed-pwa-v76';
 const CORE_CACHE=VERSION+'-core';
 const RUNTIME_CACHE=VERSION+'-runtime';
 const CORE=['./','./index.html','./manifest.webmanifest'];
