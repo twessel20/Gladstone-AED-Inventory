3944e1ff903915278a8357561a87d166f9a3160a
twessel20 <twessel20@aol.com>
2026-09-24 11:48:56 -0500

    Switch report PDF sharing to direct jsPDF generator v89

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const VERSION='gfd-aed-pwa-v88';
+const VERSION='gfd-aed-pwa-v89';
 const CORE_CACHE=VERSION+'-core';
 const RUNTIME_CACHE=VERSION+'-runtime';
-const CORE=['./','./index.html','./manifest.webmanifest','./report-builder.js'];
+const CORE=['./','./index.html','./manifest.webmanifest','./report-builder.js','./pdf-export.js'];
 
 self.addEventListener('install',event=>{
   self.skipWaiting();
@@ -25,7 +25,9 @@ async function withReportBuilder(response){
   const type=response.headers.get('content-type')||'';
   if(!type.includes('text/html'))return response;
   let html=await response.text();
-  if(!html.includes('report-builder.js'))html=html.replace('</body>','<script src="./report-builder.js?v=88"></script></body>');
+  if(!html.includes('report-builder.js'))html=html.replace('</body>','<script src="./report-builder.js?v=89"></script></body>');
+  if(!html.includes('jspdf.umd.min.js'))html=html.replace('</body>','<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script></body>');
+  if(!html.includes('pdf-export.js'))html=html.replace('</body>','<script src="./pdf-export.js?v=89"></script></body>');
   const headers=new Headers(response.headers);
   headers.delete('content-length');
   return new Response(html,{status:response.status,statusText:response.statusText,headers});
